7ee6686f4a343edab31d313393d315705472061f
Latifa-Ahmed111 <latifaa.hanfy@gmail.com>
2025-05-07 13:40:36 +0300

    UnitOfWork

---
+++ Core/DomainLayer/Contracts/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using DomainLayer.Models;
+using DomainLayer.Models.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Contracts
+{
+    public  interface IUnitOfWork
+    {
+
+        //IGenericReposatory<Product,int>ProductRepo { get; }
+        Task<int> SaveChangesAsync();
+
+        IGenericReposatory<TEntity, Tkey> GetReposatory<TEntity, Tkey>() 
+            where TEntity : BaseEntity<Tkey>;
+       
+
+    }
+}
