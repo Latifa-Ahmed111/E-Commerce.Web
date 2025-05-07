19992f8ddda8b0bbee83f6bd4dbc28752ca53c51
Latifa-Ahmed111 <latifaa.hanfy@gmail.com>
2025-05-07 16:07:05 +0300

    Product Controller

---
+++ InFrastructre/Presenation/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presenation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController(ISevicesManger ServiceManger ):ControllerBase
+    {
+        //GetallProduct 
+        [HttpGet]
+        //GetUrl/api/Products
+        public async Task<ActionResult<IEnumerable< ProductDto>>>GetAllProducts()
+        {
+
+            var Products = await ServiceManger.ProductService.GetAllProductsAsync();
+
+            return Ok(Products);
+        }
+
+        [HttpGet("brands")]
+        //GetUrl/api/Products
+        public async Task<ActionResult<IEnumerable<TypeDto>>> GetAllBrands()
+        {
+
+            var Brands = await ServiceManger.ProductService.GetAllBrandsAsync();
+
+            return Ok(Brands);
+        }
+
+        [HttpGet("types")]
+        //GetUrl/api/Products
+        public async Task<ActionResult<IEnumerable<TypeDto>>> GetAllTypes()
+        {
+
+            var Types = await ServiceManger.ProductService.GetAllTypesAsync();
+
+            return Ok(Types);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDto>>GetProductById(int id )
+        {
+
+            var Product = await ServiceManger.ProductService.GetProductByIdAsync(id);
+            return Product;
+
+        }
+
+    }
+}
